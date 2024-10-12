696ad01cfab2f6e14fecab8e5e16120514b09cf1
Lauren <laurennadonna@gmail.com>
2024-10-12 11:32:26 -0400

    react webpage

---
+++ webpage/src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+//import logo from './logo.svg';
+import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path ="/" element={<Home />} />
+          <Route path ="/about" element={<About />} />
+          <Route path ="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ webpage/src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact()
+{
+    return(
+        <div>
+            <h1>Contact Page</h1>
+            <p>Welcome to Contact!</p>
+        </div>
+    );
+}
+
+export default Contact;
