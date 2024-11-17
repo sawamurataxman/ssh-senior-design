57ea24c5e777b70c556265b424e1e96b4f42e80e
Lauren <laurennadonna@gmail.com>
2024-11-16 20:43:16 -0500

    added brightness

--- frontend/webpage/src/pages/Home.js
+++ frontend/webpage/src/pages/Home.js
@@ -5,23 +5,57 @@ function Home()
 {
     const [color, setColor] = useState('#fff')
     const [showColorPicker, setShowColorPicker] = useState(false)
+    const [brightness, setBrightness] = useState("ON")
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const data ={
+            brightness,
+            color: color.hex,
+            
+        };
+        fetch("http://localhost:3000/api/smartbulb",)(
+        {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json",
+            },
+            body: JSON.stringify(data),
+
+        })
+        .then(response => response.json())
+        .then(result => console.log("Data sent:", result))
+        .catch(error => console.error("Error:", error));
+    };
+
     return(
         <div>
-            <h1>Home Page</h1>
-            <p>Welcome to the home Page!</p>
-        <div>
-            <button onClick = {()=> setShowColorPicker(showColorPicker => !showColorPicker)}>
-            {showColorPicker ? ' Close color' : 'Click to pick a color'}
-            </button>
-            {showColorPicker && (
-                <ChromePicker
-                color={color}
-                onChange={updatedColor => setColor(updatedColor)}
-            />
-            )}
-            <div style = {{background: color, height: '100px', width: '100px', marginTop: '10px'}}>
+            <h1>Bulb Page</h1>
+            <p>Welcome to the Bulb Page!</p>
+            <div>
+                <button onClick = {()=> setShowColorPicker(showColorPicker => !showColorPicker)}>
+                {showColorPicker ? ' Close color' : 'Click to pick a color'}
+                </button>
+                {showColorPicker && (
+                    <ChromePicker
+                    color={color}
+                    onChange={updatedColor => setColor(updatedColor)}
+                />
+                )}
+                <div style = {{background: color, height: '100px', width: '100px', marginTop: '10px'}}>
+                </div>
             </div>
-        </div>
+            <div>
+                <label>Brightness Settings </label>
+                <input
+                    type="range"
+                    min="0"
+                    max="255"
+                    value={brightness}
+                    onChange ={(e) => setBrightness(e.target.value)}
+                />
+            </div>
         </div>
     );
 }
